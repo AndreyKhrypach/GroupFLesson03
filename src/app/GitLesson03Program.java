fbf4020499cd448fe8e45bd913e59c7331e29299
AndreyKhrypach <pacik78@gmail.com>
2024-09-20 20:57:19 +0300

    Add new system out println

--- src/app/GitLesson03Program.java
+++ src/app/GitLesson03Program.java
@@ -4,6 +4,7 @@ public class GitLesson03Program {
 
     public static void main(String[] args){
         System.out.println("Hello git");
+        System.out.println("Second changes for git");
     }
 
 }
